f5a795901d0e1152c61d2063d66647c111fa0b5c
Vlastimil Klik <vlastimil.klik@rem.cz>
2017-11-18 15:12:40 +0100

    Flash messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,17 @@
 		<div class="column">
 
 			<h4 class="ui dividing header">@lang('messages.signIn')</h4>
-			<form class="ui form" method="POST" action="{{ route('login') }}">
+
+			<form class="ui form @if($errors->any())error @endif" method="POST" action="{{ route('login') }}">
+				@if($errors->any())
+					<div class="ui error message">
+						<div class="header">@lang('auth.form_title')</div>
+						<ul class="list">
+							<li>{{ $errors->first() }}</li>
+						</ul>
+					</div>
+				@endif
+
 				{{ csrf_field() }}
 				<div class="field">
 					<label for="username" class="">@lang('messages.usernameOrEmail')</label>
