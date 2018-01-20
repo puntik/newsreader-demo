24e6d53ea3126b9a2f1dd2a999673ef2ac7cc189
Vlastimil Klik <vlastimil.klik@gmail.com>
2018-01-20 22:48:50 +0100

    Cleaning routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ Route::get('/', '\App\Http\Controllers\WelcomeController');
 
 Route::get('/ca/{id}-{name}', '\App\Http\Controllers\HelloController')->name('category');
 
-\Illuminate\Support\Facades\Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/search', 'SearchResultsController')->name('searchResult');
 
 Route::get('/login/github', 'Auth\LoginController@redirectToProvider');
 Route::get('/login/github/callback', 'Auth\LoginController@handleProviderCallback');
+
+// Authentication Routes...
+// Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
